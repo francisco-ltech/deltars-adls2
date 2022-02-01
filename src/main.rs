7f6df50f05234fddc6a031ea352c0cd4c4617933
Francisco Ruiz A <francisco@ltech.io>
2022-02-01 10:48:56 +0000

    Working version of reading delta table from ADLS2.

--- src/main.rs
+++ src/main.rs
@@ -2,20 +2,27 @@ extern crate anyhow;
 extern crate deltalake;
 
 #[tokio::main(flavor = "current_thread")]
-async fn main() -> anyhow::Result<()> {
+async fn read_my_delta() -> anyhow::Result<()> {
     // The following ENV_VARS must be set:
-    // E.g.: export AZURE_STORAGE_ACCOUNT_NAME=wdpstoragedev.dfs.core.windows.net
-    // E.g.: export AZURE_STORAGE_ACCOUNT_KEY=mtPS4dX0lkHCgYCjcfdmLZEshY+5uWFhwqXhav1m7hUWWYzfD/fO0Zv6FhjIPIYcKKlr2vkIZeHPLRNc8+ztqQ==
+    // E.g.: export AZURE_STORAGE_ACCOUNT_NAME=
+    // E.g.: export AZURE_STORAGE_ACCOUNT_KEY=
 
     let account = std::env::var("AZURE_STORAGE_ACCOUNT_NAME").unwrap();
     let table_uri = format!("adls2://{}/{}/{}/delta-0.8.0/", account, "sandbox", "francisco");
-    println!("{}", table_uri);
     
     let table = deltalake::open_table(table_uri.as_str())
             .await
             .unwrap();
+
+    
     
     println!("{}", table);
     
     Ok(())
+}
+
+
+fn main() {
+    let r = read_my_delta();
+    println!("{:?}", r);
 }
